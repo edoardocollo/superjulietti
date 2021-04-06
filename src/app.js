d9fb7b2eb11363613a53d24909dc3c708cca59c8
edoardo <edoardo.collo@gmail.com>
2021-04-06 12:32:19 +0200

    add navigation to feedback page

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router'
 
 Vue.component('carousel', require('../Carousel.vue').default);
 Vue.component('carousel2', require('../Carousel2.vue').default);
-Vue.use(VueRouter)
 
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-import Carousel from '../Carousel.vue';
-const routes = [
-  { path: '/foo', component: Carousel },
-  { path: '/bar', component: Bar }
-]
-
-const router = new VueRouter({
-  routes // short for `routes: routes`
-})
 let app = new Vue({
-  router,
   el: '#app',
   data:{
-
+    toggle:true,
+  },
+  methods:{
+    toggleFunction: function(){
+      this.toggle = !this.toggle;
+    }
   },
 });
