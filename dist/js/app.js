723707ab5431e31234296d7a50033ff94c2781ee
Mirko <mirko.saponaro91@gmail.com>
2021-04-07 13:05:17 +0200

    Bobobobo

--- dist/js/app.js
+++ dist/js/app.js
@@ -682,7 +682,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:70%;\nmargin:auto;\nheight:240px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width:90%;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:70%;\r\nmargin:auto;\r\nheight:240px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width:90%;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -705,7 +705,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:100%;\nmargin:auto;\nheight:240px;\nbackground-color: #d4fec9;\nmargin: 0 20px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width: 90% ;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:100%;\r\nmargin:auto;\r\nheight:240px;\r\nbackground-color: #d4fec9;\r\nmargin: 0 20px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width: 90% ;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -728,7 +728,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:100%;\nmargin:auto;\nheight:240px;\nbackground-color: #d4fec9;\nmargin: 0 20px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width: 90% ;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:100%;\r\nmargin:auto;\r\nheight:240px;\r\nbackground-color: #d4fec9;\r\nmargin: 0 20px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width: 90% ;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -751,7 +751,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:100%;\nmargin:auto;\nheight:240px;\nbackground-color: #d4fec9;\nmargin: 0 20px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width: 90% ;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:100%;\r\nmargin:auto;\r\nheight:240px;\r\nbackground-color: #d4fec9;\r\nmargin: 0 20px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width: 90% ;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -774,7 +774,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:100%;\nmargin:auto;\nheight:240px;\nbackground-color: #d4fec9;\nmargin: 0 20px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width: 90% ;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:100%;\r\nmargin:auto;\r\nheight:240px;\r\nbackground-color: #d4fec9;\r\nmargin: 0 20px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width: 90% ;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -797,7 +797,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:100%;\nmargin:auto;\nheight:240px;\nbackground-color: #d4fec9;\nmargin: 0 20px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width: 90% ;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:100%;\r\nmargin:auto;\r\nheight:240px;\r\nbackground-color: #d4fec9;\r\nmargin: 0 20px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width: 90% ;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -820,7 +820,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\nwidth:100%;\nmargin:auto;\nheight:240px;\nbackground-color: #d4fec9;\nmargin: 0 20px;\n}\n.slide {\n  display:flex;\n  justify-content:center;\n  align-items:center;\n}\n.slide > img {\n  width: 90% ;\n  border:7px solid #c2e1ca;\n  margin:auto;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#carousel_box {\r\nwidth:100%;\r\nmargin:auto;\r\nheight:240px;\r\nbackground-color: #d4fec9;\r\nmargin: 0 20px;\n}\n.slide {\r\n  display:flex;\r\n  justify-content:center;\r\n  align-items:center;\n}\n.slide > img {\r\n  width: 90% ;\r\n  border:7px solid #c2e1ca;\r\n  margin:auto;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
